4a535f2ef978cd6950b5065d4c7d7d95a67c1be6
Kevin Gleneagles <kevin.gleneagles@student.umn.ac.id>
2021-11-24 11:33:09 +0700

    tutorial 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';// import juga model article yang telah dibangun
+// isi from pada import merupakkan path karena article model ada di folder article maka
+// kita harus masuk ke folder article dulu, baru ambil article.model ts
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'modul11-tutorial1';
+  title = 'angular-object-model';
+  articles: Article[];
+
+  constructor(){
+    // kita deklarasi saja data awal pada constructor dengan membuat object article 
+    this.articles = [ new Article(this.title: 'Angular 2', 
+      link: 'https://angular.io',
+      votes: 10),
+    new Article(title: 'FullStack', link:'https://fullstack.io', votes:2),
+    new Article(title: 'Angular Homepage', link:'https://angular.io', votes:1),
+
+  ];
+  }
+  // untuk tambah article
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
+    console.log('Adding article title: ${title.value} and link: ${link.value}');
+    this.articles.push(new Article(title.value, link.value, votes:0));
+    this.value = '';
+    link.value ='';
+    return false;
+  }
+  // untuk mensort article 
+  sortedArticle(): Article[] {
+    return this.articles.sort(compareFn:(a:Article, b:Article) => b.votes - a.votes );
+  }
 }
---
+++ src/app/article/article.model.ts
@@ -0,0 +1,39 @@
+import { ThrowStmt } from '@angular/compiler';
+
+export class Article {
+  title: string; // untuk title article
+  link: string; // untuk link pada article nantinya
+  votes: number; //untuk jumlah vote pada article
+
+  // kita bangun constructor pembentuk model, karena nantinya
+  // article akan bisa bertambah ketika ditekan submit link
+  constructor(title: string, link: string, votes?: number) {
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+
+  //article nantinya akan memilih vote up dan vote down
+  // seperti forum pada reddit
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+  domain(): string{
+      // function domain ini akan membuat kita mengambil link utama dari web
+      // https://google.com/angular7, setelah dilakukan split hasilnya
+      // akan menjadi www.google.com mulai dari slash akan dipotong(split)
+
+      try{
+          const domainAndPath: string = this.link.split(separator '//')[1];
+          return domainAndPath.split(separator '/')[0];
+      }
+      catch (err){
+          return null;
+      }
+  }
+}
